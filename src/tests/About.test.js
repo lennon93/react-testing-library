0afc5bd7b8d79e511f965f994058c5f11f7fdf27
Lennon <lennon_lxs@hotmail.com>
2023-01-20 18:39:07 -0300

    cria test se contem a imagem da pokedex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -21,6 +21,8 @@ describe('Teste o componente <About.js />', () => {
   });
   test('Teste se a página contém uma imagem da Pokédex', () => {
     renderWithRouter(<About />);
-    screen.getByRole('img', { src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png' });
+    const image = screen.getByRole('img', { name: /pokédex/i });
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
